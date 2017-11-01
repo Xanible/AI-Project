702be2810a4bf8b5ac194cb254c25862ccf8b201
breeo <breeo@DESKTOP-96H2STE>
2017-11-01 10:27:55 -0500

    Added competition and cook classes

---
+++ src/main/Competition.java
@@ -0,0 +1,10 @@
+// CSC 416
+// Semester Project
+
+//Competition class to handle each of the 3 competition rounds
+
+package main;
+
+public class Competition {
+
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,13 @@
+// CSC 416
+// Semester Project
+
+//Main class to run the application and set up the user interface
+
 package main;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
